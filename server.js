efb11f31246394da2f87c826cb186b0586ef25db
chumpon-asaneerat <chumponsenate@gmail.com>
2019-12-03 13:05:55 +0700

    Cleanup server.js code.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+//#region express and middlewares
+
 const express = require("express");
 
 const morgan = require("morgan");
@@ -8,68 +10,14 @@ const bodyparser = require("body-parser");
 const cookieparser = require("cookie-parser");
 const favicon = require("serve-favicon");
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
+//#endregion
 
-const APPNAME = "Express Project";
+const APPNAME = "socket.io sample";
 const PORT = 3000;
 
 const app = express();
 
-const useStatusMonitor = () => {
-app.use(require('express-status-monitor')({
-        title: APPNAME + ' Status', // Default title
-        //theme: 'default.css',     // Default styles
-        path: '/status',
-        //socketPath: '/socket.io', // In case you use a custom path
-        //websocket: existingSocketIoInstance,
-        spans: [{
-            interval: 1,            // Every second
-            retention: 60           // Keep 60 data points in memory
-        }, {
-            interval: 5,            // Every 5 seconds
-            retention: 60
-        }, {
-            interval: 15,           // Every 15 seconds
-            retention: 60
-        }],
-        chartVisibility: {
-            cpu: true,
-            mem: true,
-            load: true,
-            responseTime: true,
-            rps: true,
-            statusCodes: true
-        },
-        //ignoreStartsWith: '/admin',
-        healthChecks: []
-    }));
-};
-
-const sendDummyRequest = () => {
-    let request = require('request');
-    let requestUrl = `http://localhost:${PORT}/`;
-    let interval = 50;
-    let sendRequestHomeUrl = () => {
-        setTimeout(() => {
-            const code = 200 + Math.random() * 399;
-            request.get(`${requestUrl}`);
-            sendRequestHomeUrl();
-          }, interval);
-    };
-    sendRequestHomeUrl();
-};
-
-//## [ Status Monitor ] ===================================================##
-//
-// useStatusMonitor function need to call first before another middlewares.
-//
-// comment out this line in production.
-//
-//useStatusMonitor(); // tell app express app to use status monitor.
-//
-//
-//## [ Status Monitor ] ===================================================##
+//#region use middlewares
 
 app.use(helmet());
 app.use(morgan("dev"));
@@ -97,59 +45,14 @@ const dist_libs = [
     /* jQuery UI */
     { "route": "/dist/css", "path": "jquery-ui-1.12.1" },
     { "route": "/dist/js", "path": "jquery-ui-1.12.1" },
-    /* bootstrap 4.x */
-    { "route": "/dist/js", "path": "popperjs-1.15.0" },
-    { "route": "/dist/js", "path": "tooltipjs-1.3.2" },
-    { "route": "/dist", "path": "bootstrap-4.2.1" },
     /* font-awesome 5.x */
     { "route": "/dist", "path": "font-awesome-5.9.0" },
-    /* emoji-symbols */
-    { "route": "/dist/css", "path": "emoji-symbols-1.0.0" },
-    /* flag-icon-css 3.x */
-    { "route": "/dist", "path": "flag-icon-css-3.1.0" },
-    /* animate-css */
-    { "route": "/dist", "path": "animate-css-3.7.2" },
     /* moment */
     { "route": "/dist/js", "path": "moment-2.24.0" },
-    /* chartjs */
-    { "route": "/dist", "path": "chart-js-2.8.0" },
-    /* chartjs-plugin-datalabels */
-    { "route": "/dist", "path": "chart-js-plugin-datalabels-0.6.0" },
-    /* chartjs-plugin-piechart-outlabels */
-    { "route": "/dist", "path": "chart-js-plugin-piechart-outlabels-0.1.4" },
-    /* howler */
-    { "route": "/dist/js", "path": "howler-2.1.2" },
-    /* jquery-org-chart */
-    { "route": "/dist", "path": "jquery-org-chart-2.1.3" },
-    /* tabulator */
-    { "route": "/dist", "path": "tabulator-4.4.1" },
-    /* ace */
-    { "route": "/dist/js", "path": "ace-1.4.5" },
-    /* simplebar */
-    { "route": "/dist", "path": "simplebar-4.1.0" },
-    /* overlay-scrollbar */
-    { "route": "/dist", "path": "overlay-scrollbars-1.9.1" },
-    /* reveal.js */
-    //{ "route": "/dist", "path": "reveal-3.8.0" },
     /* riotjs */
     { "route": "/dist/js", "path": "riotjs-3.13.2" },
-    /* fabricjs */
-    { "route": "/dist", "path": "fabricjs-3.4.0" },
-    /* gifuct */
-    { "route": "/dist", "path": "gifuct-js-1.0.0" },
-    /* highcharts */
-    { "route": "/dist/css", "path": "highcharts-7.2.0/code/css" },
-    { "route": "/dist/js", "path": "highcharts-7.2.0/code" },
-    /* interact.js */
-    { "route": "/dist/js", "path": "interactjs-1.6.2" },
-    /* gifler */
-    { "route": "/dist", "path": "gifler-0.3.0" },
-    /* videojs */
-    { "route": "/dist/css", "path": "videojs-7.6.5" },
-    { "route": "/dist/js", "path": "videojs-7.6.5" },
-    /* jquery-easyui */
-    { "route": "/dist/css", "path": "jquery-easyui-1.8.7" },
-    { "route": "/dist/js", "path": "jquery-easyui-1.8.7" },
+    /* socket.io */
+    { "route": "/dist/js", "path": "socket.io-2.2.0" },
     /* for access precompile riotjs component */
     { "route": "/components", "path": "../../dist/component/riot" }
 ];
@@ -164,100 +67,14 @@ dist_libs.forEach(element => {
     dist_lib(app, element.route, element.path);
 });
 
+//#endregion
+
 app.get("/", (req, res) => {
-    res.status(200).send(`It's work!!!`);
+    res.json({
+        data: "it's work!!"
+    })
 });
-
-app.get("/:file", (req, res, next) => {
-    if (req.params.file === 'index.html') {
-        res.sendFile(path.join(__dirname, req.params.file));
-    }
-    else {
-        next();
-    }
-});
-
-/**
- * @swagger
- * /test:
- *   post:
- *     description: Login 123
- *     consumes:
- *       - application/json
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: test
- *         description: Items list array
- *         in: formData
- *         required: false
- *         type: array
- *         collectionFormat: multi
- *         items:
- *            type: "integer"
- *       - name: profileId
- *         description: Password
- *         in: formData
- *         required: true
- *         type: string
- *       - name: file
- *         description: File To Upload
- *         in: formData
- *         required: false
- *         type: file
- *     responses:
- *       200:
- *         description: OK result in json object.
- */
-app.post('/test', function(req, res) {
-	console.log('req', req)
-	res.status(200).json({ status: 'OK'});
-});
-
-/**
- * @swagger
- * /bar:
- *   get:
- *     description: home get api
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: result in json object.
- */
-app.get('/bar', function(req, res) { 
-    res.status(200).json({ status: 'OKISH'}); 
-});
-
-// set swagger (used jsdoc)
-const swaggerSpec = swaggerJSDoc({
-    swaggerDefinition: {
-        info: {
-            title: 'My App API',
-            version: '1.0.0'
-        }
-    },
-    //apis: ['./routes/index.js']
-    apis: ['./server.js']
-});
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// set swagger (used external json)
-/*
-const swaggerDocument = require('./swagger.json');
-let swaggerOpts = { explorer: false };
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOpts));
-*/
 
 const server = app.listen(PORT, () => {
     console.log(`${APPNAME} listen on port: ${PORT}`);
 });
-
-//## [ Status Monitor ] ===================================================##
-// Code for test express status monitor.
-//
-// comment out if not used.
-//
-//sendDummyRequest(); // make a fake request.
-//
-//## [ Status Monitor ] ===================================================##
